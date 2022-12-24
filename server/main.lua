5c74b9509e3a79651223370b19cc4f4f644a0ee5
SYNO® <89760730+SYNO-SY@users.noreply.github.com>
2022-12-24 14:46:47 +0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -9,11 +9,11 @@ AddEventHandler('carfixstation:costRepair', function(cost)
 	if xPlayer.getMoney() >= Config.cost then		
 		xPlayer.removeMoney(Config.cost)
 		TriggerClientEvent('carfixstation:fixCar', _source)
-		--TriggerClientEvent('esx:showNotification', _source, _U('cost_repair', Config.cost))
+		--TriggerClientEvent('esx:showNotification', _source, _U('cost_repair', Config.cost)) --(use this for default notification)
 		TriggerClientEvent('SY_Notify:Alert', _source, "Reapir Station", _U('cost_repair', Config.cost), 5000, 'info')
 	else
 		TriggerClientEvent('SY_Notify:Alert', _source, "Reapir Station", _U('not_enough_money'), 5000, 'error')
-		--TriggerClientEvent('esx:showNotification', _source, _U('not_enough_money'))
+		--TriggerClientEvent('esx:showNotification', _source, _U('not_enough_money')) --(use this for default notification)
 	end
 end)
 
