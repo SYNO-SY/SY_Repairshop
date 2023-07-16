698b0e997768eb6b9af0ff3439b31446b559955f
SYNO® <89760730+SYNO-SY@users.noreply.github.com>
2023-07-16 15:53:04 +0400

    some bug fixes!!

--- client/main.lua
+++ client/main.lua
@@ -2,19 +2,20 @@ ESX = exports["es_extended"]:getSharedObject()
 local fixing = false
 local lastZone, currentAction
 
-CreateThread (function()	
-    while true do
+CreateThread(function()
+	while true do
 		Wait(0)
-		if not IsPedInAnyVehicle(PlayerPedId(), false) then 
-		    Wait(1000)
+		if not IsPedInAnyVehicle(PlayerPedId(), false) then
+			Wait(1000)
 		end
 		if IsPedInAnyVehicle(PlayerPedId(), false) then
 			local playerPed = PlayerPedId()
 			local pos = GetEntityCoords(playerPed, true)
-			local isInMarker, currentZone, letSleep = false, nil, true	
-			for k,v in pairs(Config.Stations) do
+			local isInMarker, currentZone, letSleep = false, nil, true
+			local PedInVehicle = GetVehiclePedIsIn(playerPed, false)
+			for k, v in pairs(Config.Stations) do
 				if not fixing then
-					if(Vdist(pos.x, pos.y, pos.z, v.x, v.y, v.z) < 15) then
+					if (Vdist(pos.x, pos.y, pos.z, v.x, v.y, v.z) < 15) then
 						letSleep = false
 						if IsPedInAnyVehicle(playerPed, false) then
 							isInMarker, currentZone = true, k
@@ -24,20 +25,23 @@ CreateThread (function()
 			end
 			if (isInMarker and not hasAlreadyEnteredMarker) or (isInMarker and lastZone ~= currentZone) then
 				hasAlreadyEnteredMarker, lastZone = true, currentZone
-				showtextui()
+				if GetPedInVehicleSeat(PedInVehicle, -1) then
+					showtextui()
+				end
 				TriggerEvent('sy_repairshop:hasEnteredMarker', currentZone)
 			end
-	
+
 			if not isInMarker and hasAlreadyEnteredMarker then
 				hasAlreadyEnteredMarker = false
-				hidetextui()	
+				hidetextui()
+				currentAction = nil
 			end
 			if letSleep then
 				Wait(500)
 			end
 		end
-    end
-    Wait(1000)
+	end
+	Wait(1000)
 end)
 
 
@@ -49,7 +53,7 @@ CreateThread(function()
 				if currentAction == 'dorepair' then
 					TriggerServerEvent('carfixstation:costRepair', Config.cost)
 				end
-				
+
 				currentAction = nil
 			end
 		else
@@ -59,7 +63,7 @@ CreateThread(function()
 end)
 
 AddEventHandler('sy_repairshop:hasEnteredMarker', function(zone)
-	currentAction  = 'dorepair'
+	currentAction = 'dorepair'
 end)
 
 
@@ -68,15 +72,15 @@ AddEventHandler('carfixstation:fixCar', function()
 	local playerPed = PlayerPedId()
 	local vehicle = GetVehiclePedIsIn(playerPed, false)
 	local fixing = true
-	TriggerEvent('carfixstation:markAnimation')	
+	TriggerEvent('carfixstation:markAnimation')
 	FreezeEntityPosition(vehicle, true)
-	sendNotification(_U('repair_processing'), 'info', false, nil, Config.RepairTime-700 )
+	sendNotification(_U('repair_processing'), 'info', false, nil, Config.RepairTime - 700)
 	if Config.EnableSoundEffect then
-	    TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 5.0, 'car_repair', 0.7)
+		TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 5.0, 'car_repair', 0.7)
 	end
 	DoorControl(4)
-        if Config.progressBar == 'ESX' then
-	    ESX.Progressbar(_U('repair_processing'), Config.RepairTime)
+	if Config.progressBar == 'ESX' then
+		ESX.Progressbar(_U('repair_processing'), Config.RepairTime)
 	elseif Config.progressBar == 'ox' then
 		lib.progressBar({
 			duration = Config.RepairTime,
@@ -87,8 +91,8 @@ AddEventHandler('carfixstation:fixCar', function()
 				car = true,
 			},
 		})
-        end
-	sendNotification(_U('repair_finish'),"success",false,nil,5000)
+	end
+	sendNotification(_U('repair_finish'), "success", false, nil, 5000)
 	local fixing = false
 	SetVehicleFixed(vehicle)
 	SetVehicleDeformationFixed(vehicle)
@@ -109,13 +113,13 @@ end
 
 if Config.Blips then
 	CreateThread(function()
-		for i=1, #Config.Stations, 1 do
+		for i = 1, #Config.Stations, 1 do
 			local blip = AddBlipForCoord(Config.Stations[i].x, Config.Stations[i].y, Config.Stations[i].z)
 
-			SetBlipSprite (blip, 643)
+			SetBlipSprite(blip, 643)
 			SetBlipDisplay(blip, 4)
-			SetBlipScale  (blip, 0.6)
-			SetBlipColour (blip, 45)
+			SetBlipScale(blip, 0.6)
+			SetBlipColour(blip, 45)
 			SetBlipAsShortRange(blip, true)
 
 			BeginTextCommandSetBlipName("STRING")
